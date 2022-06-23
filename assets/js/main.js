237ffcf7751f7dc7ccfaef63b21725bc03c01ccf
Robert Anisimov <skillup23@yandex.ru>
2022-06-23 18:05:31 +0300

    Заготовки

---
+++ assets/js/main.js
@@ -0,0 +1,138 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+// var swiperPopular = new Swiper(".popular__container", {
+//   spaceBetween: 32,
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: 'auto',
+//   loop: true,
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+
+/*=============== VALUE ACCORDION ===============*/
+const accordionItem = document.querySelectorAll('.value__accordion-item')
+
+accordionItem.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordion-header')
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+
+    toggleItem(item)
+
+    if(openItem && openItem !== item) {
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion-content')
+
+  if(item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+// const sr = ScrollReveal ({
+//   origin: 'top',
+//   distance: '60px',
+//   duration: 2500,
+//   delay: 400,
+//   reset: true
+// })
+
+// sr.reveal(`.home__title, .popular__container, .subscribe__container, .footer__container`)
+// sr.reveal(`.home__description, .footer__info`, {delay: 500})
+// sr.reveal(`.home__search`, {delay: 600})
+// sr.reveal(`.home__value`, {delay: 700})
+// sr.reveal(`.home__images`, {delay: 800, origin: 'bottom'})
+// sr.reveal(`.logos__img`, {interval: 100})
+// sr.reveal(`.value__images, .contact__content`, {origin: 'left'})
+// sr.reveal(`.value__content, .contact__images`, {origin: 'right'})
+
+
+/*=============== Telephone mask ===============*/
+window.addEventListener("DOMContentLoaded", function() {
+  [].forEach.call( document.querySelectorAll('.home__search-input'), function(input) {
+  var keyCode;
+  function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, ""),
+          new_value = matrix.replace(/[_\d]/g, function(a) {
+              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+          });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+          i < 5 && (i = 3);
+          new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function(a) {
+              return "\\d{1," + a.length + "}"
+          }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5)  this.value = ""
+  }
+
+  input.addEventListener("input", mask, false);
+  input.addEventListener("focus", mask, false);
+  input.addEventListener("blur", mask, false);
+  input.addEventListener("keydown", mask, false)
+
+});
+
+});
